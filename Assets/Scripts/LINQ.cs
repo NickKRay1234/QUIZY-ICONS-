c59b54119e238e74d11bf60a69c01a677da6454c
Mykola Kalchuk <80714127+NickKRay1234@users.noreply.github.com>
2023-04-07 00:15:14 +0200

    LINQ - let;

--- Assets/Scripts/LINQ.cs
+++ Assets/Scripts/LINQ.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -16,23 +17,64 @@ public class LINQ : MonoBehaviour
         }
     }
 
+    private void Awake()
+    {
+        IEnumerable<Person> people = new List<Person>
+        {
+            new("Tom", 23),
+            new("Bob", 27)
+        };
+
+        var personel = 
+            from p in people 
+            select new
+            {
+                FirstName = p.Name,
+                Year = DateTime.Now.Year - p.Age
+            };
+
+        // foreach (var p in personel)
+        //     Debug.Log($"{p.FirstName} - {p.Year}");
+    }
+
     private void Start()
     {
         var people = new List<Person>()
         {
-            new Person("Tom", 23),
-            new Person("Bob", 27),
-            new Person("Sam", 29),
-            new Person("Alice", 24)
+            new("Tom", 23),
+            new("Bob", 27),
+            new("Sam", 29),
+            new("Alice", 24)
         };
+
+        var newPersonal = from p in people
+            let name = $"Mr. {p.Name}"
+            let year = DateTime.Now.Year - p.Age
+            select new
+            {
+                Name = name,
+                Year = year
+            };
+
+        var oldPersonal = from fired in people
+            let name = $"Mr. {fired.Name} is fired"
+            let year = DateTime.Now.Year - fired.Age
+            select new
+            {
+                Name = name,
+                Year = year
+            };
+
+        foreach (var personal in oldPersonal)
+            Debug.Log($"{personal.Name} - {personal.Year}");
 
         var names = from p in people select p.Name;
         var ages = from p in people select p.Age;
 
-        foreach (string n in names)
-            Debug.Log(n);
-
-        foreach (int age in ages)
-            Debug.Log(age);
+        // foreach (string n in names)
+        //     Debug.Log(n);
+        //
+        // foreach (int age in ages)
+        //     Debug.Log(age);
     }
 }
